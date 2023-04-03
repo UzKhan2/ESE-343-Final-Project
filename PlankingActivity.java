6a9431f0d47ab37fa6c59896a434e4d19bed5aa6
UzKhan2 <99924800+UzKhan2@users.noreply.github.com>
2023-04-02 23:49:10 -0400

    Update PlankingActivity.java

--- PlankingActivity.java
+++ PlankingActivity.java
@@ -32,7 +32,8 @@ public class PlankingActivity extends Activity implements SensorEventListener {
     private boolean wasRunning;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_planking);
         onFloor = 0;
@@ -41,8 +42,8 @@ public class PlankingActivity extends Activity implements SensorEventListener {
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         prox = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
-        if (savedInstanceState != null) {
-
+        if (savedInstanceState != null)
+        {
             // Get the previous state of the stopwatch
             // if the activity has been
             // destroyed and recreated.
@@ -59,7 +60,8 @@ public class PlankingActivity extends Activity implements SensorEventListener {
     }
 
     @Override
-    protected void onResume() {
+    protected void onResume()
+    {
         super.onResume();
         sensorManager.registerListener(this, prox, SensorManager.SENSOR_DELAY_NORMAL);
         if (wasRunning) {
@@ -68,7 +70,8 @@ public class PlankingActivity extends Activity implements SensorEventListener {
     }
 
     @Override
-    protected void onPause() {
+    protected void onPause()
+    {
         super.onPause();
         sensorManager.unregisterListener(this);
         wasRunning = running;
@@ -77,22 +80,27 @@ public class PlankingActivity extends Activity implements SensorEventListener {
 
     @SuppressLint("SetTextI18n")
     @Override
-    public void onSensorChanged(SensorEvent event) {
+    public void onSensorChanged(SensorEvent event)
+    {
         float count = event.values[0];
-        if(count < 2 && onFloor == 0 && firstTry){
+        if(count < 2 && onFloor == 0 && firstTry)
+        {
             onFloor = 1;
             running = false;
             firstTry = false;
         }
-        else if(count < 2 && onFloor == 0){
+        else if(count < 2 && onFloor == 0)
+        {
             onFloor = 1;
             running = false;
             firstTime = false;
         }
-        else if(count > 9 && onFloor == 1){
+        else if(count > 9 && onFloor == 1)
+        {
             onFloor = 0;
             //start timer
-            if(firstTime == true) {
+            if(firstTime == true)
+            {
                 running = true;
                 runTimer();
             }
@@ -100,13 +108,12 @@ public class PlankingActivity extends Activity implements SensorEventListener {
     }
 
     @Override
-    public void onAccuracyChanged(Sensor sensor, int i) {
-
+    public void onAccuracyChanged(Sensor sensor, int i)
+    {
     }
 
     @Override
-    public void onSaveInstanceState(
-            Bundle savedInstanceState)
+    public void onSaveInstanceState(Bundle savedInstanceState)
     {
         savedInstanceState
                 .putInt("seconds", seconds);
@@ -118,49 +125,52 @@ public class PlankingActivity extends Activity implements SensorEventListener {
 
     private void runTimer()
     {
-            // Get the text view.
-            final TextView timeView = findViewById(R.id.timeDisplay);
+        // Get the text view.
+        final TextView timeView = findViewById(R.id.timeDisplay);
+        // Creates a new Handler
+        final Handler handler = new Handler();
 
-            // Creates a new Handler
-            final Handler handler = new Handler();
 
+        handler.post(new Runnable()
+        {
+            @Override
 
-            handler.post(new Runnable() {
-                @Override
+            public void run()
+            {
+                int hours = seconds / 3600;
+                int minutes = (seconds % 3600) / 60;
+                int secs = seconds % 60;
 
-                public void run() {
-                    int hours = seconds / 3600;
-                    int minutes = (seconds % 3600) / 60;
-                    int secs = seconds % 60;
+                // Format the seconds into hours, minutes, and seconds.
+                String time
+                        = String
+                        .format(Locale.getDefault(),
+                                "%d:%02d:%02d", hours,
+                                minutes, secs);
 
-                    // Format the seconds into hours, minutes, and seconds.
-                    String time
-                            = String
-                            .format(Locale.getDefault(),
-                                    "%d:%02d:%02d", hours,
-                                    minutes, secs);
+                // Set the text view text.
+                timeView.setText(time);
 
-                    // Set the text view text.
-                    timeView.setText(time);
-
-                    // If running is true, increment the
-                    // seconds variable.
-                    if (running) {
-                        seconds++;
-                    }
-
-                    // Post the code again
-                    // with a delay of 1 second.
-                    handler.postDelayed(this, 1000);
+                // If running is true, increment the
+                // seconds variable.
+                if (running)
+                {
+                    seconds++;
                 }
-            });
+                // Post the code again
+                // with a delay of 1 second.
+                handler.postDelayed(this, 1000);
+            }
+        });
     }
 
-    public void saveResult(View view) {
+    public void saveResult(View view)
+    {
         //Save seconds to database (Uzair handling this)
     }
 
-    public void returnToMain(View view) {
+    public void returnToMain(View view)
+    {
         seconds = 0;
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
