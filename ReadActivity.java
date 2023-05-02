86a560c6b6bf946f247b7af706473d792d9248c8
Khan <99924800+UzKhan2@users.noreply.github.com>
2023-05-02 03:36:13 -0400

    Update ReadActivity.java

--- ReadActivity.java
+++ ReadActivity.java
@@ -8,17 +8,13 @@ import android.widget.RelativeLayout;
 import android.widget.Toast;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.util.ArrayList;
 
 public class ReadActivity extends AppCompatActivity {
@@ -28,44 +24,47 @@ public class ReadActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private LinearLayoutManager layoutManager;
     private ReadRecyclerAdapter recyclerAdapter;
+    private ArrayList<Book> fitList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_read);
 
-        readProgressLayout = findViewById(R.id.readProgressLayout);
-        readProgressBar = findViewById(R.id.readProgressBar);
-        recyclerView = findViewById(R.id.recyclerView);
-        layoutManager = new LinearLayoutManager(this);
+        readProgressLayout=findViewById(R.id.readProgressLayout);
+        readProgressBar=findViewById(R.id.readProgressBar);
+        recyclerView=findViewById(R.id.recyclerView);
+        layoutManager= new LinearLayoutManager(this);
 
-        ArrayList<Book> bookList = new ArrayList<>();
-
-        String url = "https://script.google.com/macros/s/AKfycbyXe3cAdQ6tmIEvH9MKRxGyY-yuuWzCMYsKkZyK9qLb_0_STbWaJOqqbZx5oa9E1p6q/exec";
-
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+        final JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
+                "https://script.google.com/macros/s/AKfycbyXe3cAdQ6tmIEvH9MKRxGyY-yuuWzCMYsKkZyK9qLb_0_STbWaJOqqbZx5oa9E1p6q/exec",
+                null,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
                         readProgressLayout.setVisibility(View.GONE);
                         readProgressBar.setVisibility(View.GONE);
+
                         try {
-                            JSONArray data = response.getJSONArray("bookList");
-                            for (int i = 0; i < data.length(); i++) {
-                                JSONObject bookJasonObject = data.getJSONObject(i);
+                            JSONArray data = response.getJSONArray("fitList");
+                            for(int i=0; i<data.length(); i++){
+                                JSONObject bookJsonObject = data.getJSONObject(i);
                                 Book bookObject = new Book(
-                                        bookJasonObject.getString("itemName"),
-                                        bookJasonObject.getString("itemAuthor"),
-                                        bookJasonObject.getInt("itemPrice")
+                                        bookJsonObject.getString("personName"),
+                                        bookJsonObject.getString("personActivity"),
+                                        bookJsonObject.getInt("activityData")
                                 );
-                                bookList.add(bookObject);
+                                fitList.add(bookObject);
                             }
-                            recyclerAdapter = new ReadRecyclerAdapter(ReadActivity.this, bookList);
+                            recyclerAdapter= new ReadRecyclerAdapter(ReadActivity.this,fitList);
                             recyclerView.setAdapter(recyclerAdapter);
                             recyclerView.setLayoutManager(layoutManager);
+
                         } catch (JSONException e) {
                             e.printStackTrace();
+                            Toast.makeText(ReadActivity.this, e.toString(), Toast.LENGTH_SHORT).show();
                         }
+
                     }
                 },
                 new Response.ErrorListener() {
@@ -75,18 +74,9 @@ public class ReadActivity extends AppCompatActivity {
                         readProgressBar.setVisibility(View.GONE);
                         Toast.makeText(ReadActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
                     }
-                }
-        ) {
-            @Override
-            public java.util.Map<String, String> getHeaders() {
-                try {
-                    return super.getHeaders();
-                } catch (AuthFailureError e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        };
+                });
 
         Volley.newRequestQueue(this).add(jsonObjectRequest);
+
     }
 }
